8ceb2ae312a1aaad1ab88fef47083c8f7e0a0722
theta1118 <1311134412@qq.com>
2021-12-23 10:01:51 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import Nav from '@/components/Nav.vue';
+import Layout from '@/components/Layout.vue';
 
 Vue.config.productionTip = false
 
 //全局引入
 Vue.component('Nav',Nav)
+Vue.component('Layout',Layout);
 
 new Vue({
   router,
