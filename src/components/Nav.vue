d602f7e71bcb1bf426564d9bb53fe1b4e54a020a
theta1118 <1311134412@qq.com>
2021-12-23 15:27:54 +0800

    update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,12 @@
 <template>
 <!--  class后面接一个属性选择器，就只会影响到这一个div-->
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money"/>
+      </svg>
+      记账
+    </router-link>
     ｜
     <router-link to="/labels">标签</router-link>
     ｜
@@ -10,15 +15,20 @@
 </template>
 
 <script lang="ts">
-export default {
-name: "Nav"
-}
+
+  let importAll = (requireContext:__WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try{
+    importAll(require.context('../assets/icons',true,/\.svg$/));
+  }catch (error){
+    console.log(error);
+  }
+
+  export default {
+    name: "Nav"
+  };
 </script>
 
 <!--scoped是指有范围的，这个Nav只在当前组件起作用-->
 <style lang="scss" scoped>
-  .nav{
-    border: 1px solid red;
-  }
-
+  .nav{border: 1px solid red;}
 </style>
