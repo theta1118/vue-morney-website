25acb9782699cde2d50ec548c0eab5e1383ccc0c
theta1118 <1311134412@qq.com>
2021-12-26 22:57:42 +0800

    使用jS实现了一个组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'"
+      @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'"
+      @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-name: "Types"
+<script>
+  export default {
+  name: 'Types',
+    props:['xxx'], //1、外部属性
+    data(){ //1、内部属性
+      return{
+        type:'-'// '-'表示支出，'+'表示收入
+      }
+    },
+    mounted() { //4、生命周期
+      console.log(this.xxx)
+    },
+    methods:{ //3、方法
+      selectType(type){ //type只能是'-'和'+'中的一个
+        if(type !=='-' && type !=='+'){
+          throw new Error('type is unknown')
+        }
+        this.type = type
+      }
+    }
+
+
 }
 </script>
 
