160e7249c05268d0f1fb2c98bb0c0cc0c9092189
theta1118 <1311134412@qq.com>
2021-12-28 10:56:08 +0800

    可以选中或取消 tag

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,18 +3,24 @@
     <NumberPad/>
     <Types :xxx="  999  "/>
     <Notes/>
-    <Tags/>
+    <Tags :data-source="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
+
 export default {
   name: 'Money',
   components: {Tags, Notes, Types, NumberPad},
+  data(){
+    return{
+      tags:['衣','食','住','行','彩票']
+    }
+  }
 };
 </script>
 
