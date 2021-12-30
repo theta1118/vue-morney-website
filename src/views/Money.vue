d3271da1159d87456cf788254c98f4c845b450f8
theta1118 <1311134412@qq.com>
2021-12-30 10:14:13 +0800

    把record的复制保存到list

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
   <Layout class-prefix="layout">
     {{record}}
-    <NumberPad @update:value="onUpdateAmount"/>
+<!--    触发这个submit的点击事件-->
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -17,7 +18,7 @@
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Watch} from 'vue-property-decorator';
 
   //如何在ts里面声明一个类型
 
@@ -42,7 +43,8 @@
     components: {Tags, Notes, Types, NumberPad},
   })
   export default class Money extends Vue{
-      tags=['衣','食','住','行','彩票']
+      tags=['衣','食','住','行','彩票'];
+      recordList:Record[]=[];
       record:Record ={
         tags:[],notes:'',type:'-',amount:0
       };
@@ -55,6 +57,22 @@
     }
     onUpdateAmount(value:string){
         this.record.amount = parseFloat(value);
+    }
+
+    saveRecord(){
+      const record2 = JSON.parse(JSON.stringify(this.record));
+      this.recordList.push(record2);
+      console.log(this.recordList);
+      // localStorage.set('recordList',JSON.stringify(this.recordList));
+      //把recordList保存到localstorage里面就可以了
+      //用JSON.stringify把它序列化一下，把recordList序列化一下，但是这个方法不太好
+      // 如果你在别的地方点击，不一定会保存，所以使用watch方法更好
+      //record2拥有和record完全一样的属性，保存的时候永远保存的是它的副本
+    }
+
+    @Watch('recordList')
+    onRecordListChange(){
+      window.localStorage.setItem('recordList',JSON.stringify(this.recordList));
     }
   }
 </script>
