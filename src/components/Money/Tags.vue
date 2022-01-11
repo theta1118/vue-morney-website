75b72d5aab537c589f08e1ffcf28f5ada02f5034
theta1118 <1311134412@qq.com>
2022-01-11 22:14:00 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,14 +19,16 @@
   @Component({
       computed:{
         tagList(){
-          //TODO
-          // return this.$store.fetchTags();
-          return []
+          return this.$store.state.tagList;
         }
       }
     })
   export default class Tags extends Vue{
     selectedTags: string[] = [];
+
+    created(){
+      this.$store.commit('fetchTags');
+    }
 
     toggle(tag:string){
       const index = this.selectedTags.indexOf(tag);
@@ -41,8 +43,7 @@
     create(){
       const name = window.prompt('请输入标签名');
       if (!name){return window.alert('标签名不能为空')}
-      //TODO 意思是一会儿要改
-      // store.createTag(name);
+      this.$store.commit('createTag',name);
     }
   }
 </script>
