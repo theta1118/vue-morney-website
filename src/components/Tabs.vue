a61f4ea557502f880b60ad35e2b8d8a0fe3cd84d
theta1118 <1311134412@qq.com>
2022-01-13 15:23:00 +0800

    更新 CSS

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,6 +1,6 @@
 <template>
-  <ul class="tabs">
-    <li v-for="item in dataSource" :key="item.value"
+  <ul class="tabs" :class="{[classPrefix+'-tabs']:classPrefix}">
+    <li v-for="item in dataSource" :key="item.value" class="tabs-item"
         :class="liClass(item)" @click="select(item)">{{ item.text }}
     </li>
   </ul>
@@ -22,7 +22,6 @@
     @Prop(String)
     classPrefix?: string;
 
-    //会放到对象上
     liClass(item:DataSourceItem){
       return{
         [this.classPrefix + '-tab-item']: this.classPrefix,
@@ -30,7 +29,6 @@
       };
     };
 
-    //会放到原型上，优先使用这一种
     select(item: DataSourceItem) {
       this.$emit('update:value', item.value);
     }
@@ -38,30 +36,30 @@
 </script>
 
 <style lang="scss" scoped>
-.tabs {
-  background: #c4c4c4;
-  display: flex;
-  text-align: center;
-  font-size: 24px;
-
-  > li {
-    width: 50%;
-    line-height: 64px;
+  .tabs {
+    background: #c4c4c4;
     display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
+    text-align: center;
+    font-size: 24px;
 
-    &.selected::after {
-      content: '';
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 4px;
-      background: #333;
+    &-item {
+      width: 50%;
+      line-height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+      }
     }
   }
-}
 
 </style>
