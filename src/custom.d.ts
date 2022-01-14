b6e3cdb0ebf955f3439db2fec38bdedda9d18f79
theta1118 <1311134412@qq.com>
2022-01-14 22:08:56 +0800

    完成列表的 css

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,3 +1,9 @@
+type RootState = {
+  recordList: RecordItem[],
+  tagList: Tag[],
+  currentTag?: Tag
+}
+
 type RecordItem = {
   //key:类型
   //如果有初始值，就不用加类型
@@ -5,7 +11,7 @@ type RecordItem = {
   notes:string
   type:string
   amount:number  //除了写数据类型（七种），也可以写类，在js里面，类也叫做构造函数
-  createdAt?:Date //类 /构造函数，createdAt可以不存在
+  createdAt?:string //类 /构造函数，createdAt可以不存在
   // createdAt:Date | undefined;//也可以写成上面那种方式
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -6,11 +6,6 @@ import router from '@/router';
 
 Vue.use(Vuex); //把 store 绑到 Vue.prototype.$store=store
 
-type RootState = {
-  recordList: RecordItem[],
-  tagList: Tag[],
-  currentTag?: Tag
-}
 const store = new Vuex.Store({
   state: {//data
     recordList: [],
@@ -56,7 +51,7 @@ const store = new Vuex.Store({
     },
     createRecord(state, record) {
       const record2: RecordItem = clone(record);
-      record2.createdAt = new Date();
+      record2.createdAt = new Date().toISOString();
       state.recordList.push(record2);
       store.commit('saveRecords');
     },
