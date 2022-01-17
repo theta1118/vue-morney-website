e28fba4949a4985e3edf908f1c6ed70804323928
theta1118 <1311134412@qq.com>
2022-01-17 14:45:08 +0800

    deploy

---
+++ precache-manifest.4f5a027b8e82c09a09a31eef51cb92ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf042a282687070323af",
+    "url": "/vue-morney-website/css/app.6e505fe6.css"
+  },
+  {
+    "revision": "5e87cc057471f441311acf436feae47b",
+    "url": "/vue-morney-website/index.html"
+  },
+  {
+    "revision": "cf042a282687070323af",
+    "url": "/vue-morney-website/js/app.9bac54bf.js"
+  },
+  {
+    "revision": "7f66cfdb3364071bbe66",
+    "url": "/vue-morney-website/js/chunk-vendors.86b921de.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/vue-morney-website/manifest.json"
+  },
+  {
+    "revision": "30c3d36204b8bade19bbd72f5fa41103",
+    "url": "/vue-morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-morney-website/robots.txt"
+  }
+]);
