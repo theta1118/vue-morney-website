69152eb73cd3e1900c449310e1d75156806b1874
theta1118 <1311134412@qq.com>
2022-02-01 14:48:05 +0800

    自己封装 Chart 组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,20 +1,21 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-      <ol v-if="groupedList.length>0">
-        <li v-for="(group,index) in groupedList" :key="index">
-          <h3 class="title">{{beautify(group.title)}} <span>¥{{group.total}}</span></h3>
-          <ol>
-            <li v-for="item in group.items" :key="item.id"
-            class="record"
-            >
-              <span>{{tagString(item.tags)}}</span>
-              <span class="notes">{{item.notes}}</span>
-              <span>¥{{item.amount}}</span>
-            </li>
-          </ol>
-        </li>
-      </ol>
+    <Chart :options="x"/>
+    <ol v-if="groupedList.length>0">
+      <li v-for="(group,index) in groupedList" :key="index">
+        <h3 class="title">{{ beautify(group.title) }} <span>¥{{ group.total }}</span></h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record"
+          >
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>¥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
     <div v-else class="noResult">
       目前没有相关记录
     </div>
@@ -22,81 +23,108 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
-  import Tabs from '@/components/Tabs.vue';
-  import recordTypeList from '@/constants/recordTypeList';
-  import dayjs from 'dayjs';
-  import clone from '@/lib/clone';
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import recordTypeList from '@/constants/recordTypeList';
+import dayjs from 'dayjs';
+import clone from '@/lib/clone';
+import Chart from '@/components/Chart.vue';
 
-  @Component({
-    components: {Tabs},
-  })
-  export default class Statistics extends Vue {
-    tagString(tags:Tag[]){
-      return tags.length === 0? '无' :
-        tags.map(t=>t.name).join('，');
-    }
-
-    beautify(string:string){
-      const day = dayjs(string);
-      const now = dayjs();
-      if(day.isSame(now,'day')){
-        return '今天' ;
-      }else if(day.isSame(now.subtract(1,'day'),'day')){
-        console.log('hi');
-        return '昨天';
-      }else if(day.isSame(now.subtract(2,'day'),'day')){
-        return '前天';
-      }else if(day.isSame(now,'year')){
-        return day.format('M月D日');
-      }else{
-        return day.format('YYYY年M月D日');
-      }
-    }
-
-    get recordList(){
-      return (this.$store.state as RootState).recordList;
-    }
-
-    get groupedList(){
-      const {recordList} = this;
-
-      const newList = clone(recordList)
-        .filter(r=>r.type===this.type)
-        .sort((a,b)=>dayjs(b.createdAt).valueOf()-dayjs(a.createdAt).valueOf());
-      if(newList.length===0){return [];}
-      type Result = {title:string,total?:number,items:RecordItem[]}[]
-      const result:Result = [{title:dayjs(newList[0].createdAt).format('YYYY-MM-DD'),items:[newList[0]]}];
-      for(let i =1;i<newList.length;i++){
-        const current = newList[i];
-        const last = result[result.length-1];
-        if(dayjs(last.title).isSame(dayjs(current.createdAt),'day')){
-          last.items.push(current);
-        }else{
-          result.push({title:dayjs(current.createdAt).format('YYYY-MM-DD'),items:[current]});
-        }
-      }
-      result.forEach(group=>{
-        group.total = group.items.reduce((sum,item)=>sum+item.amount,0);
-      });
-      return result;
-    }
-
-    mounted(){
-      this.$store.commit('fetchRecords');
-    }
-
-    type = '-';
-    recordTypeList = recordTypeList;
+@Component({
+  components: {Tabs, Chart},
+})
+export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' :
+      tags.map(t => t.name).join('，');
   }
+
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      console.log('hi');
+      return '昨天';
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日');
+    } else {
+      return day.format('YYYY年M月D日');
+    }
+  }
+
+  get x() {
+    return {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }],
+      tooltip: {show: true}
+    };
+  }
+
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get groupedList() {
+    const {recordList} = this;
+
+    const newList = clone(recordList)
+      .filter(r => r.type === this.type)
+      .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+    if (newList.length === 0) {
+      return [];
+    }
+    type Result = { title: string, total?: number, items: RecordItem[] }[]
+    const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+      }
+    }
+    result.forEach(group => {
+      group.total = group.items.reduce((sum, item) => sum + item.amount, 0);
+    });
+    return result;
+  }
+
+  mounted() {
+    this.$store.commit('fetchRecords');
+  }
+
+  type = '-';
+  recordTypeList = recordTypeList;
+}
 </script>
 
 <style scoped lang="scss">
-.noResult{
+.echarts {
+  max-width: 100%;
+  height: 100%;
+}
+
+.noResult {
   padding: 16px;
   text-align: center;
 }
+
 ::v-deep {
   .type-tabs-item {
     background: #f4da75;
@@ -109,24 +137,29 @@
       }
     }
   }
+
   .interval-tabs-item {
     height: 48px;
   }
-  %item{
+
+  %item {
     padding: 8px 16px;
     line-height: 24px;
     display: flex;
     justify-content: space-between;
     align-content: center;
   }
-  .title{
+
+  .title {
     @extend %item;
   }
-  .record{
+
+  .record {
     background: white;
     @extend %item;
   }
-  .notes{
+
+  .notes {
     margin-right: auto;
     margin-left: 16px;
     color: #999;
